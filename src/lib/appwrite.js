9e1b0e1de0573f169afacef668e884715f3ad6eb
Mayank Dixit <mayankdixit121@gmail.com>
2026-03-05 11:05:46 +0530

    restructured project folder and updated categories component

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,10 @@
 import { Client, Databases, Account } from "appwrite";
 
 const client = new Client();
+
 client
   .setEndpoint("https://sgp.cloud.appwrite.io/v1")
-  .setProject("69a829bb002bf6ce3a84");
+  .setProject("69a8ef2800253a49e2c8");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
-
